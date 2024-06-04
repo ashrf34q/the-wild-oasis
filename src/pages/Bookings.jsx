e3c82ec834fa60d93c73a9a789eb638fc07a8478
Ashraf Makki <makkiashraf08@gmail.com>
2024-06-03 21:51:43 -0400

    Added react hot toast for toast messages

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -5,6 +6,9 @@ function Bookings() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All bookings</Heading>
+      <button onClick={() => toast.loading("Loading Bookings")}>
+        Click me
+      </button>
       <p>TEST</p>
     </Row>
   );
